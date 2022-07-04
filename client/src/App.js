710ff61aba1ee61f2f0552921cdf7fba051195ca
Ramin Esmzad <ramin.esmzad@hotmail.com>
2022-07-04 08:09:11 +0430

    PostList component in React

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
     return <div className='container'>
         <h1>Simple Blog Using Microservices</h1>
         <h2>Create Post</h2>
         <PostCreate />
+        <hr />
+        <PostList />
     </div>;
 };
--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -6,13 +6,16 @@ export default () => {
     const onSubmit = async (event) => {
         event.preventDefault();
         await axios.post('http://localhost:4000/posts', {title});
+        setTitle('');
     };
+
     return <div>
         <form onSubmit={onSubmit}>
             <div className='form-group'>
                 <label>Title</label>
                 <input value={title} onChange={e => setTitle(e.target.value)} className='form-control'></input>
             </div>
+            <br />
             <button className='btn btn-primary'>Submit</button>
         </form>
     </div>;
